918e3b566dda76e75bc1e3644edeccac9dd0b1f4
ciach <ci3l4k@gmail.com>
2023-10-16 19:28:40 +0200

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pages-vite',
+  base: '/pages-vite/',
   plugins: [react()],
 })
